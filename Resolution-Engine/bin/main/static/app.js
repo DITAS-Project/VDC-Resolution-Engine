8dd7e91b6c82ce5afcc007ac52688bfea9f846d0
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-04 14:52:23 +0300

    Add angular controller searchController

--- Resolution-Engine/bin/main/static/app.js
+++ Resolution-Engine/bin/main/static/app.js
@@ -1,1 +1,22 @@
 var app = angular.module('app', []);
+
+app.controller("searchController", function($scope, $http, $rootScope, parseService){
+	
+	$rootScope.search = false;
+	$scope.search = function(){
+		
+		$http({
+			method: 'POST',
+			url: "/searchBP",     
+			data: $scope.searchText,
+			headers: {'Content-Type': 'application/json'}
+		})
+		.then(function successCallback(response) {
+			$rootScope.search = true
+			parseService.parseResults(response.data)
+		}, function errorCallback(response) {
+			console.log(response)
+		});
+	}
+
+});
