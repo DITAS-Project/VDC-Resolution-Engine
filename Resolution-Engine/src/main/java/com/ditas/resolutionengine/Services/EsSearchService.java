3a73149517bfb6adefe0dc3c31e71ea575f128f3
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-04 15:25:48 +0300

    add java service responsible for searching in elastic-search

---
+++ Resolution-Engine/src/main/java/com/ditas/resolutionengine/Services/EsSearchService.java
@@ -0,0 +1,13 @@
+package com.ditas.resolutionengine.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EsSearchService {
+	
+	public String blueprintSearch(String searchText) {
+		
+		return null;
+	}
+
+}
