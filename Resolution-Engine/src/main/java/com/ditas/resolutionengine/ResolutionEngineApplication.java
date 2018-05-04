3d6d98cc7c416d41c41850e4da27b1e030710959
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-04 12:42:15 +0300

    Starting project for resolution engine

---
+++ Resolution-Engine/src/main/java/com/ditas/resolutionengine/ResolutionEngineApplication.java
@@ -0,0 +1,12 @@
+package com.ditas.resolutionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolutionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolutionEngineApplication.class, args);
+	}
+}
---
+++ Resolution-Engine/src/test/java/com/ditas/resolutionengine/ResolutionEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ditas.resolutionengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResolutionEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
