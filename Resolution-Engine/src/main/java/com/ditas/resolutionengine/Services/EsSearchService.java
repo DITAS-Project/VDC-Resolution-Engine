bd8881543fa91fd93f047e551502824d2e77e3a2
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-07 15:26:29 +0300

    Update search service class

--- Resolution-Engine/src/main/java/com/ditas/resolutionengine/Services/EsSearchService.java
+++ Resolution-Engine/src/main/java/com/ditas/resolutionengine/Services/EsSearchService.java
@@ -1,11 +1,39 @@
 package com.ditas.resolutionengine.Services;
 
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ditas.resolutionengine.Configurations.ElasticSearchConfig;
 
 @Service
 public class EsSearchService {
 	
+	@Autowired
+	ElasticSearchConfig config;
+	
 	public String blueprintSearch(String searchText) {
+		try {
+			Client client = config.client();
+			
+			QueryBuilder qb = boolQuery();
+			
+			SearchResponse response = client.prepareSearch("ditas")
+					.setTypes("blueprints")
+					.setQuery(qb)
+					.execute().actionGet();
+			
+			
+			return response.toString();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
