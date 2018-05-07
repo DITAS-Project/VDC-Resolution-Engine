f76007729e81623f27e5cdfa6d4051ec9b8312e5
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-07 15:21:44 +0300

    Configure ElasticSearch client in configuration class

--- Resolution-Engine/src/main/java/com/ditas/resolutionengine/Configurations/ElasticSearchConfig.java
+++ Resolution-Engine/src/main/java/com/ditas/resolutionengine/Configurations/ElasticSearchConfig.java
@@ -1,5 +1,38 @@
 package com.ditas.resolutionengine.Configurations;
 
-public class ElasticSearchConfig {
+import java.net.InetAddress;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+	@Value("${elasticsearch.host}")
+    private String EsHost;
+
+    @Value("${elasticsearch.port}")
+    private int EsPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String EsClusterName;
+
+
+   @Bean
+    public Client client() throws Exception {
+
+        Settings esSettings = Settings.settingsBuilder()
+                .put("cluster.name", EsClusterName)
+                .build();
+        return TransportClient.builder()
+                .settings(esSettings)
+                .build()
+                .addTransportAddress(
+				  new InetSocketTransportAddress(InetAddress.getByName(EsHost), EsPort));
+   
+    }
 }
