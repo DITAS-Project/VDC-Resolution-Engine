e1a6f8281d9945015e8215d7566c162544260274
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-07 15:43:26 +0300

    Update ElasticSearch query in search service class

--- Resolution-Engine/src/main/java/com/ditas/resolutionengine/Services/EsSearchService.java
+++ Resolution-Engine/src/main/java/com/ditas/resolutionengine/Services/EsSearchService.java
@@ -1,10 +1,13 @@
 package com.ditas.resolutionengine.Services;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.nestedQuery;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.support.QueryInnerHitBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +23,11 @@ public class EsSearchService {
 		try {
 			Client client = config.client();
 			
-			QueryBuilder qb = boolQuery();
+			QueryBuilder qb = boolQuery()
+					.should(matchQuery("description", searchText))
+					.should(nestedQuery("tags", matchQuery("tags.tags", searchText).boost(8))
+							.innerHit(new QueryInnerHitBuilder().setFetchSource("method_name", null))
+					);
 			
 			SearchResponse response = client.prepareSearch("ditas")
 					.setTypes("blueprints")
