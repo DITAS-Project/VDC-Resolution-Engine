39f4ee926f9de35d16ab9b49c9568685ab126874
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-07 15:14:46 +0300

    Update Blueprint Controller

--- Resolution-Engine/src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
+++ Resolution-Engine/src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
@@ -1,16 +1,26 @@
 package com.ditas.resolutionengine.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ditas.resolutionengine.Services.EsSearchService;
+
 @RestController
 public class BlueprintController {
+	
+	@Autowired
+	EsSearchService searchService;
 	
 	@RequestMapping(method=RequestMethod.POST , value="/searchBP")
 	public String searchBP(@RequestBody String searchText){
 		
-		return null;
+		System.out.println("Search text: "+searchText);
+		String response = searchService.blueprintSearch(searchText);
+		
+		return response;
 	}
 }
