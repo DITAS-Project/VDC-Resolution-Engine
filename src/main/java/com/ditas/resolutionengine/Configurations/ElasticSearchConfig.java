ed8fbc2a3501818ba8521369e0812f5e025a6177
Thodoris Florakis <thodwrisfl@gmail.com>
2018-05-17 18:48:39 +0300

    Import ShiedPlugin class

--- src/main/java/com/ditas/resolutionengine/Configurations/ElasticSearchConfig.java
+++ src/main/java/com/ditas/resolutionengine/Configurations/ElasticSearchConfig.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.shield.ShieldPlugin;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
