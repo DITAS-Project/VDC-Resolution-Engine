c0473671f32d14ad018fa3c6279948e6066d5a42
Thodoris Florakis <thodwrisfl@gmail.com>
2018-09-21 18:08:12 +0300

    Add individual DURE request controller

--- src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
+++ src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.ditas.resolutionengine.Entities.Requirements;
+import com.ditas.resolutionengine.Services.DURERequestService;
 import com.ditas.resolutionengine.Services.EsSearchService;
 import com.ditas.resolutionengine.Services.ResolutionEngineService;
 
@@ -21,6 +22,9 @@ public class BlueprintController {
 	
 	@Autowired
 	ResolutionEngineService resolutionService;
+	
+	@Autowired
+	DURERequestService dureService;
 	
 	@RequestMapping(method=RequestMethod.POST , value="/searchBlueprint")
 	public String searchBP(@RequestBody String searchText){
@@ -70,6 +74,27 @@ public class BlueprintController {
 		
 		
 		return esResponse;
+	
+	
+	}
+	
+	@RequestMapping(method=RequestMethod.POST , value="/searchBlueprintByReq_DureRequest")
+	public String searchBPByRequirements_DureRequest(@RequestBody String  applicationRequirements){
+		
+		Requirements requirements = new Requirements();
+		
+		JSONObject applicationRequirements_json = new JSONObject(applicationRequirements);
+		
+		org.json.JSONArray method_tags = applicationRequirements_json.getJSONObject("functionalRequirements").getJSONArray("methodTags");
+		requirements.setMethodTags(method_tags);
+		
+		org.json.JSONArray vdc_tags =  applicationRequirements_json.getJSONObject("functionalRequirements").getJSONArray("vdcTags");
+		requirements.setVdcTags(vdc_tags);
+	
+		String esResponse = searchService.blueprintSearchByReq(requirements);
+		String dureRequest = dureService.createRequest(esResponse, applicationRequirements_json);
+		
+		return dureRequest;
 	
 	
 	}
