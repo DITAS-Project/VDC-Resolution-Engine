1823a6ec95eef261116db75c33eb9cf42c018a22
Thodoris Florakis <thodwrisfl@gmail.com>
2018-10-29 14:33:25 +0200

    Add controller for intermidiate blueprint testing

--- src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
+++ src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
@@ -1,8 +1,14 @@
 package com.ditas.resolutionengine.Controllers;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -96,6 +102,60 @@ public class BlueprintController {
 		
 		return dureRequest;
 	
+	
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.GET , value="/GetIntermediateBlueprint")
+	public String GetIntermediateBlueprint(){
+		
+		String requirements_file = "";
+		Resource resource = new ClassPathResource("user_reqs.json");
+		try {
+			InputStream resourceInputStream = resource.getInputStream();
+			int data = resourceInputStream.read();
+			while(data != -1) {
+				  requirements_file += (char) data;
+				  data =  resourceInputStream.read();
+				}
+			
+			
+			Requirements requirements = new Requirements();
+			
+			JSONObject applicationRequirements_json = new JSONObject(requirements_file);
+			
+			org.json.JSONArray method_tags = applicationRequirements_json.getJSONObject("functionalRequirements").getJSONArray("methodTags");
+			requirements.setMethodTags(method_tags);
+			
+			org.json.JSONArray vdc_tags =  applicationRequirements_json.getJSONObject("functionalRequirements").getJSONArray("vdcTags");
+			requirements.setVdcTags(vdc_tags);
+		
+			String response = resolutionService.ResolutionEngineRequest(requirements, applicationRequirements_json);
+			
+			JSONArray response_json = new JSONArray(response);
+			return response_json.getJSONObject(0).getJSONObject("blueprint").toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+		/*
+		Requirements requirements = new Requirements();
+		
+		JSONObject applicationRequirements_json = new JSONObject(applicationRequirements);
+		
+		org.json.JSONArray method_tags = applicationRequirements_json.getJSONObject("functionalRequirements").getJSONArray("methodTags");
+		requirements.setMethodTags(method_tags);
+		
+		org.json.JSONArray vdc_tags =  applicationRequirements_json.getJSONObject("functionalRequirements").getJSONArray("vdcTags");
+		requirements.setVdcTags(vdc_tags);
+	
+		String esResponse = searchService.blueprintSearchByReq(requirements);
+		String dureRequest = dureService.createRequest(esResponse, applicationRequirements_json);
+		
+		return dureRequest;
+	*/
 	
 	}
 }
