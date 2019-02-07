c5aae91289705f5a574ba898ef5f58dacd7de229
achilleasmarinakis <achmarin@mail.ntua.gr>
2019-02-07 17:49:15 +0200

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Run the application
-java -jar /app/Resolution-Engine-0.0.1-SNAPSHOT.jar
+java -jar /app/Resolution-Engine-0.0.1-SNAPSHOT.jar --spring.application.json='{"host":$DOCKER_HOST_IP}'
