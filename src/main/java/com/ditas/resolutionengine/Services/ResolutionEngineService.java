cc5482f91cf60f4296ad057938a73abf64a8bba9
Vaggelis Psomakelis <vpsomak@mail.ntua.gr>
2019-03-06 13:28:44 +0200

    Revert "Revert "Addition of Similarity Rating""
    This reverts commit 88cb27547d124e41b6f6e4f2ee0e0565350dbacb.

--- src/main/java/com/ditas/resolutionengine/Services/ResolutionEngineService.java
+++ src/main/java/com/ditas/resolutionengine/Services/ResolutionEngineService.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of VDC-Resolution-Engine.
- * 
- * VDC-Resolution-Engine is free software: you can redistribute it 
- * and/or modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation, either version 3 of the License, 
+ *
+ * VDC-Resolution-Engine is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
- * VDC-Resolution-Engine is distributed in the hope that it will be 
+ *
+ * VDC-Resolution-Engine is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with VDC-Resolution-Engine.  
+ * along with VDC-Resolution-Engine.
  * If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  * VDC-Resolution-Engine is being developed for the
  * DITAS Project: https://www.ditas-project.eu/
  */
@@ -28,20 +28,23 @@ import com.ditas.resolutionengine.Entities.Requirements;
 
 @Service
 public class ResolutionEngineService {
-	
+
 	@Autowired
 	EsSearchService esService;
-	
+
 	@Autowired
 	DURERequestService dureService;
-	
+
+	@Autowired
+	SimilarityRatingService simService;
+
 	public String ResolutionEngineRequest(Requirements requirements, JSONObject app_requirements) {
-		
 		String esResponse = esService.blueprintSearchByReq(requirements);
 		String dureRequest = dureService.createRequest(esResponse, app_requirements);
 		String dureResponse = dureService.sendRequest(dureRequest);
-				
-		return dureResponse;
+		String userRatingResponse = simService.addRanking(app_requirements,dureResponse);
+		System.out.println(userRatingResponse);
+		return userRatingResponse;
 	}
 
 }
