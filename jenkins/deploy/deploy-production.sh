19208fe64ad2b4dbedb3492cd4f6b91587193008
achilleasmarinakis <achmarin@mail.ntua.gr>
2019-04-11 14:26:47 +0300

    Update deploy-production.sh

--- jenkins/deploy/deploy-production.sh
+++ jenkins/deploy/deploy-production.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# Production environment: 178.22.69.83
-# Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem
+# IDEKO SDK production environment: 153.92.30.56
+# OSR SDK production environment: 153.92.30.225
+
 
 # This file is part of VDC-Resolution-Engine.
 # 
@@ -21,11 +22,12 @@
 # VDC-Resolution-Engine is being developed for the
 # DITAS Project: https://www.ditas-project.eu/
 
-# TODO state management? We are killing without careing about any operation the conainer could be doing.
 
-ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@178.22.69.83 << 'ENDSSH'
+# SSH to the IDEKO and deploy SDK component there
+ssh -i /opt/keypairs/ideko-sdk-key.pem cloudsigma@153.92.30.56 << 'ENDSSH'
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
-# || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
+# || true - "docker stop" fails with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0
+# Try a graceful stop: 20 seconds for SIGTERM and SIGKILL after that
 sudo docker stop --time 20 vdc-resolution-engine || true
 sudo docker rm --force vdc-resolution-engine || true
 sudo docker pull ditas/vdc-resolution-engine:production
@@ -33,7 +35,23 @@ sudo docker pull ditas/vdc-resolution-engine:production
 # Get the host IP
 HOST_IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 
+# Run the docker mapping the ports and passing the host IP via the environmental variable "DOCKER_HOST_IP"
+sudo docker run -p 50009:8080 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-resolution-engine ditas/vdc-resolution-engine:production
+ENDSSH
 
-# SET THE PORT MAPPING
-sudo docker run -p 50011:8080 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-resolution-engine ditas/vdc-resolution-engine:production
+
+# SSH to the OSR and deploy SDK component there
+ssh -i /opt/keypairs/osr-sdk-key.pem cloudsigma@153.92.30.225 << 'ENDSSH'
+# Ensure that a previously running instance is stopped (-f stops and removes in a single step)
+# || true - "docker stop" fails with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0
+# Try a graceful stop: 20 seconds for SIGTERM and SIGKILL after that
+sudo docker stop --time 20 vdc-resolution-engine || true
+sudo docker rm --force vdc-resolution-engine || true
+sudo docker pull ditas/vdc-resolution-engine:production
+
+# Get the host IP
+HOST_IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
+
+# Run the docker mapping the ports and passing the host IP via the environmental variable "DOCKER_HOST_IP"
+sudo docker run -p 50009:8080 -e DOCKER_HOST_IP=$HOST_IP --restart unless-stopped -d --name vdc-resolution-engine ditas/vdc-resolution-engine:production
 ENDSSH
