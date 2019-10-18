5083a20e138802a8aeb2d3c26960792c659d235a
alexanderPsychas <37702286+alexanderPsychas@users.noreply.github.com>
2019-10-18 17:38:59 +0300

    cors test 3

--- src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
+++ src/main/java/com/ditas/resolutionengine/Controllers/BlueprintController.java
@@ -27,10 +27,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ditas.resolutionengine.Entities.Requirements;
 import com.ditas.resolutionengine.Services.DURERequestService;
 import com.ditas.resolutionengine.Services.EsSearchService;
@@ -51,6 +48,7 @@ public class BlueprintController {
 
 
 	@RequestMapping(method=RequestMethod.POST , value="/searchBlueprintByReq")
+    @CrossOrigin(origins = "http://localhost:8080")
 	public String searchBPByRequirements(@RequestBody String  applicationRequirements){
 		
 		Requirements requirements = new Requirements();
