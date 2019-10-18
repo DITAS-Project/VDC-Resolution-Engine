827eacb78f1e3c2210a19977d4fb55e6edbe5865
achilleasmarinakis <achmarin@mail.ntua.gr>
2019-10-18 17:53:30 +0300

    Revert "test cors 2"
    This reverts commit 1c87585f0ec07c851bde54eaaec85873d784db3d.

--- src/main/java/com/ditas/resolutionengine/ResolutionEngineApplication.java
+++ src/main/java/com/ditas/resolutionengine/ResolutionEngineApplication.java
@@ -22,10 +22,8 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "*")
 public class ResolutionEngineApplication {
 
 	public static void main(String[] args) {
